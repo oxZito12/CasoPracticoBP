dd1db2f6f27b529fe9c68550a8c3a6e5d8cbe010
oxZito12 <36648552+oxZito12@users.noreply.github.com>
2026-04-29 20:15:44 -0500

    Add files via upload

---
+++ WebAppCasoPractico.Dominio/WebAppCasoPractico.Dominio/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using WebAppCasoPractico.Dominio;
+
+public class AppDbContext : System.Data.Entity.DbContext
+{
+    public AppDbContext(Microsoft.EntityFrameworkCore.DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public System.Data.Entity.DbSet<RegistroPago> PaymentIntents => Set<RegistroPago>();
+}
---
+++ WebAppCasoPractico.Dominio/WebAppCasoPractico.Dominio/RegistroPago.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAppCasoPractico.Dominio
+{
+    public class RegistroPago
+    {
+        /// <summary>
+        /// identificador
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// identificacion del cliente
+        /// </summary>
+        public string IdentificacionCliente { get; set; }
+        /// <summary>
+        /// Nombre Completo cliente
+        /// </summary>
+        public string NombreCompleto { get; set; }
+        /// <summary>
+        /// Telefono cliente
+        /// </summary>
+        public string Telefono { get; set; }
+        /// <summary>
+        /// Email cliente
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// MontoDeuda cliente
+        /// </summary>
+        public decimal MontoDeuda { get; set; }
+        /// <summary>
+        /// Monto Propuesto del cliente
+        /// </summary>
+        public decimal MontoPropuesto { get; set; }
+        /// <summary>
+        /// Dias de Mora
+        /// </summary>
+        public int DiasMora { get; set; }
+        /// <summary>
+        /// Estado 
+        /// </summary>
+        public string Estado { get; set; } = "Pendiente";
+        /// <summary>
+        /// FechaRegistro pago
+        /// </summary>
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+    }
+}
